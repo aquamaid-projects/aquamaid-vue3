3260065f2bb4af59398bed86f10e891134e8fda9
Shion Akesaka <shion.akesaka@gmail.com>
2025-04-06 11:22:45 +0900

    依存関係整理

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/test-utils', '@nuxt/eslint']
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/test-utils',
+        '@nuxt/eslint',
+        '@nuxtjs/i18n',
+        '@pinia/nuxt'
+    ],
+    i18n: {
+        // 他の設定...
+        bundle: {
+            optimizeTranslationDirective: false,  // この行を追加
+        }
+    }
 })
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue';
 // Viteの設定ファイル
 export default defineConfig({
     plugins: [vue()],
+    // Storybook 用に追加するVite設定
+    optimizeDeps: {
+        include: ['@storybook/vue3'],  // Storybook 用の依存関係を明示的に指定
+    },
 });
